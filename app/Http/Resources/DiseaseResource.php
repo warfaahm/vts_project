20a296f2770a0d11f0c23f2c3a436b5a8a66f4e2
Warfa Ahmed <warfaahmed25@gmail.com>
2023-03-21 01:00:14 +0300

    third commit - new files added (vaccine & disease)

---
+++ app/Http/Resources/DiseaseResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DiseaseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->disease_name,
+            'description' => $this->description,
+            'vaccines' => $this->vaccines,
+        ];
+    }
+}
